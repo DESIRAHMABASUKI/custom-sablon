db9f199f070fa7c65b2f25080605e942660adff5
DESIRAHMABASUKI <rakialbaniah@gmail.com>
2025-06-19 10:29:27 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+  const button = document.querySelector('.btn');
+  button.addEventListener('click', () => {
+    console.log('Tombol Hubungi Kami diklik!');
+  });
+});
